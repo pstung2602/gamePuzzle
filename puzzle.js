b8bd3e8ffd511450d21653707523fc6ec61439ed
pstung2602 <44184240+pstung2602@users.noreply.github.com>
2019-05-02 14:01:12 +0700

    baitap

---
+++ puzzle.js
@@ -0,0 +1,97 @@
+let image1 = "1_p";
+function changeImg1() {
+    let image= document.getElementById("img1");
+    if (image1 === '1_p') {
+
+        image1 = '1_c';
+
+    } else if (image1 === '1_c') {
+
+        image1 = '1_m';
+
+    } else if (image1 === '1_m') {
+
+        image1 = '1_p';
+
+    }
+    image.src =  image1 + '.jpg';
+    
+}
+let image2 = "2_p";
+function changeImg2() {
+    let image= document.getElementById("img2");
+    if (image2 === '2_p') {
+
+        image2 = '2_c';
+
+    } else if (image2 === '2_c') {
+
+        image2 = '2_m';
+
+    } else if (image2 === '2_m') {
+
+        image2 = '2_p';
+
+    }
+    image.src =  image2 + '.jpg';
+
+}
+let image3 = "3_p";
+function changeImg3() {
+    let image= document.getElementById("img3");
+    if (image3 === '3_p') {
+
+        image3 = '3_c';
+
+    } else if (image3 === '3_c') {
+
+        image3 = '3_m';
+
+    } else if (image3 === '3_m') {
+
+        image3 = '3_p';
+
+    }
+    image.src =  image3 + '.jpg';
+
+}
+let image4 = "4_p";
+function changeImg4() {
+    let image= document.getElementById("img4");
+    if (image4 === '4_p') {
+
+        image4 = '4_c';
+
+    } else if (image4 === '4_c') {
+
+        image4 = '4_m';
+
+    } else if (image4 === '4_m') {
+
+        image4 = '4_p';
+
+    }
+    image.src =  image4 + '.jpg';
+
+}
+let image5 = "5_p";
+function changeImg5() {
+    let image= document.getElementById("img5");
+    if (image5 === '5_p') {
+
+        image5 = '5_c';
+
+    } else if (image5 === '5_c') {
+
+        image5 = '5_m';
+
+    } else if (image5 === '5_m') {
+
+        image5 = '5_p';
+
+    }
+    image.src =  image5 + '.jpg';
+
+}
+
+
